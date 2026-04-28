ebf1d52a70405474e4149e1bdedbc84ce1d5d12d
rifforif <riffondiaye@gmail.com>
2026-04-28 11:55:11 +0000

    ajout classe Personne

---
+++ Personne.php
@@ -0,0 +1,43 @@
+<?php
+
+class Personne {
+    private $id;
+    private $nom;
+    private $email;
+
+    public function __construct($id, $nom, $email) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->email = $email;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function afficherInfos() {
+        echo "ID: " . $this->id . ", Nom: " . $this->nom . ", Email: " . $this->email . "\n";
+    }
+}
+
+?>
